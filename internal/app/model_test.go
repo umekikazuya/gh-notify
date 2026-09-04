73993d70f1437fdbb291f8f3b27e1463ecceeb84
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-09-04 17:11:39 +0900

    feat: View が取り得るパターンの列挙

--- internal/app/model_test.go
+++ internal/app/model_test.go
@@ -1,9 +1,11 @@
 package app_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/umekikazuya/gh-notify/internal/app"
+	"github.com/umekikazuya/gh-notify/internal/notification"
 )
 
 func TestView(t *testing.T) {
@@ -12,7 +14,71 @@ func TestView(t *testing.T) {
 		model app.Model
 		want  string
 	}{
-		{},
+		{
+			name: "default",
+			model: app.Model{
+				Notifications: []notification.Notification{
+					notificationData1,
+					notificationData2,
+				},
+				Cursor:  0,
+				Width:   0,
+				All:     false,
+				Loading: false,
+				Error:   nil,
+			},
+			want: "",
+		},
+		{
+			name: "loading",
+			model: app.Model{
+				Notifications: []notification.Notification{},
+				Cursor:        0,
+				Width:         0,
+				All:           false,
+				Loading:       true,
+				Error:         nil,
+			},
+			want: "Loading notifications.",
+		},
+		{
+			name: "notfound",
+			model: app.Model{
+				Notifications: []notification.Notification{},
+				Cursor:        0,
+				Width:         0,
+				All:           false,
+				Loading:       false,
+				Error:         nil,
+			},
+			want: "No unread notifications.",
+		},
+		{
+			name: "Error(api)",
+			model: app.Model{
+				Notifications: []notification.Notification{},
+				Cursor:        0,
+				Width:         0,
+				All:           false,
+				Loading:       false,
+				Error:         errors.New("APIエラー"),
+			},
+			want: "Error: APIエラー",
+		},
+		{
+			name: "all(`--all`)",
+			model: app.Model{
+				Notifications: []notification.Notification{
+					notificationData1, notificationData2,
+				},
+				Cursor:  0,
+				Width:   0,
+				All:     false,
+				Loading: false,
+				Error:   nil,
+			},
+			want: "",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
