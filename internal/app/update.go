defc84e5f4f70974823d355a38cf08a3f5bac9e7
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-09-06 06:47:08 +0900

    feat: キーダウン / キーアップ入力のコマンドを定義

--- internal/app/update.go
+++ internal/app/update.go
@@ -1,27 +1,39 @@
 package app
 
 import (
-	"log"
-
 	tea "charm.land/bubbletea/v2"
 )
 
-type model struct{}
-
-// Init implements [tea.Model].
-func (m *model) Init() tea.Cmd {
-	panic("unimplemented")
-}
-
 // Update implements [tea.Model].
-func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	log.Printf("%v", msg)
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		if msg.Text == "j" {
+			return m, cursorDown(m)
+		}
+		if msg.Text == "k" {
+			return m, cursorUp(m)
+		}
+	}
 	return m, nil
 }
 
-// View implements [tea.Model].
-func (m *model) View() tea.View {
-	panic("unimplemented")
+func cursorDown(m *Model) tea.Cmd {
+	return func() tea.Msg {
+		if len(m.Notifications) == m.Cursor {
+			return nil
+		}
+		m.Cursor++
+		return nil
+	}
 }
 
-var _ tea.Model = (*model)(nil)
+func cursorUp(m *Model) tea.Cmd {
+	return func() tea.Msg {
+		if m.Cursor == 0 {
+			return nil
+		}
+		m.Cursor--
+		return nil
+	}
+}
--- internal/app/update_test.go
+++ internal/app/update_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	tea "charm.land/bubbletea/v2"
+	"github.com/umekikazuya/gh-notify/internal/notification"
 )
 
 func Test_model_Update(t *testing.T) {
@@ -13,12 +14,39 @@ func Test_model_Update(t *testing.T) {
 		msg      tea.Msg
 		assertFn func(t *testing.T, m tea.Model)
 	}{
-		// TODO: Add test cases.
+		{
+			name: "",
+			seedFn: func(t *testing.T) tea.Model {
+				t.Helper()
+				return &Model{Notifications: []notification.Notification{notificationData1, notificationData2}, Cursor: 0, Width: 80, All: false, Loading: false, Error: nil}
+			},
+			msg:      tea.KeyPressMsg{Text: "j", Mod: 0, Code: 'j', ShiftedCode: 0, BaseCode: 0, IsRepeat: false},
+			assertFn: func(t *testing.T, m tea.Model) { t.Helper(); t.Logf("content\n %v", m.View().Content); t.Fatal("") },
+		},
+		{
+			name: "",
+			seedFn: func(t *testing.T) tea.Model {
+				t.Helper()
+				return &Model{Notifications: []notification.Notification{
+					notificationData1,
+					notificationData2,
+				}, Cursor: 1, Width: 80, All: false, Loading: false, Error: nil}
+			},
+			msg: tea.KeyPressMsg{Text: "k", Mod: 0, Code: 'k', ShiftedCode: 0, BaseCode: 0, IsRepeat: false},
+			assertFn: func(t *testing.T, m tea.Model) {
+				t.Helper()
+				t.Logf("content\n %v", m.View().Content)
+				t.Fatal("")
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			m := tt.seedFn(t)
-			_, _ = m.Update(tt.msg)
+			_, cmd := m.Update(tt.msg)
+			if cmd != nil {
+				cmd()
+			}
 			tt.assertFn(t, m)
 		})
 	}
