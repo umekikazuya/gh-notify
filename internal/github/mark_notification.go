c30b2c682483d371682234c2d44c8a9a082daf9c
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2026-09-08 17:22:48 +0900

    fix: CI対応

--- internal/github/mark_notification.go
+++ internal/github/mark_notification.go
@@ -39,6 +39,7 @@ func execGhApiThread(threadID string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
+	// #nosec G204
 	cmd := exec.CommandContext(
 		ctx,
 		"gh",
